700c28e24bb2f7e3fdbbcc6f5b83c8444d3cb026
RajaNareshPandugayala <rajanaresh.p@gmail.com>
2024-03-02 19:48:42 +0530

    Add customer comments file, it shows in index file

--- mail copy.php
+++ mail copy.php
@@ -20,14 +20,14 @@ $headers .= "X-Mailer: PHP". phpversion() ."\r\n" ;
 $thanksmessage = '<div style="font-family: Times New Roman, Helvetica, sans-serif;"><table width="600" style="margin:0 auto;"><tr style="background: rgb(231,241,243); background: linear-gradient(180deg, rgba(255,255,255,1) 13%, rgba(231,241,243,1) 54%);background-color: #eff2f7;"><td style="padding: 0px 21px"><img src="https://www.healingtouchpediatrics.com/assets/img/email-header-img.png" width="100%" style="padding-bottom:15px;"><div style="font-weight: 600; font-size: 16px;"><p>Dear '.$name.'</p><p>This is a quick mail to confirm your query has been received by Healing Touch Pediartics Team.</p><p>We will work on your problem and share the update quickly.</p><p style="text-align:right; margin-top:40px;">Thank You for submiting & Stay Healthy!!</p><div style="text-align:right;    width: 85%;"><a href="https://www.healingtouchpediatrics.com"><img src="https://www.healingtouchpediatrics.com/assets/img/logo_email.png" width="150"></a></div></div></td></tr></table></div>';
 
 // Determine recipient email based on location
-$recipient_email = "healingtouchpediatrics@gmail.com";
-/* if ($location === "arlington_tx76018") {
+$recipient_email = "";
+if ($location === "arlington_tx76018") {
     $recipient_email = "info@healingtouchpediatrics.com";
 } elseif ($location === "frisco_tx75033") {
     $recipient_email = "contact@prohealthstudiopediatrics.com";
 } elseif ($location === "plano_tx75024") {
     $recipient_email = "contact@sevenhillspediatrics.com";
-} */
+}
 
 
 if (!empty($recipient_email)) {
